5ec3a88c398ae6474c8a02982732cd32ca1064b5
kazakovN <krapifka102@gmail.com>
2025-05-12 22:58:59 +0300

    админка юзера

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,54 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from apps.core import models
 
 
 @admin.register(models.User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
+
     list_display = (
-        'id',
-        'username',
-        'email',
-        'fullname',
-        'phone_number',
-        'company_name',
-        'department',
-        'position',
-        'is_active',
-        'is_system',
+        'id', 'username', 'email', 'fullname', 'phone_number',
+        'company_name', 'department', 'position', 'is_active', 'is_system',
     )
     search_fields = (
-        'username',
-        'email',
-        'fullname',
-        'phone_number',
-        'company_name',
-        'department',
-        'position',
+        'username', 'email', 'fullname', 'phone_number',
+        'company_name', 'department', 'position',
     )
     list_filter = (
-        'is_active',
-        'is_system',
-        'department',
-        'position',
-        'company_name',
+        'is_active', 'is_system', 'department', 'position', 'company_name',
     )
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Личная информация', {
+            'fields': ('first_name', 'last_name', 'patronymic', 'fullname', 'email', 'avatar', 'phone_number', 'birth_date')
+        }),
+        ('Организация', {
+            'fields': ('company_name', 'department', 'position')
+        }),
+        ('Права доступа', {'fields': ('is_active', 'is_system', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
     readonly_fields = ('fullname',)
-    fields = (
-        'username',
-        'email',
-        'first_name',
-        'last_name',
-        'patronymic',
-        'fullname',
-        'avatar',
-        'phone_number',
-        'birth_date',
-        'company_name',
-        'department',
-        'position',
-        'is_active',
-    )
 
 
 @admin.register(models.Project)
